729e205e3fcc7b17f8525b9df839603d00c02dd4
Ajay Patidar <166095126+ajay90989@users.noreply.github.com>
2025-05-31 20:39:32 +0530

    Update paymentControllers.js

--- backend/App/controllers/admin/paymentControllers.js
+++ backend/App/controllers/admin/paymentControllers.js
@@ -30,99 +30,78 @@ class PaymentController {
 
 
 
-  async getPaymentsByUserId(req, res) {
-    const { userId } = req.params;
+ async getPaymentsByUserId(req, res) {
+  const { userId } = req.params;
 
-    try {
-      const UserRole = await user.findOne({ _id: userId }).select("role");
+  let UserRole = await user.findOne({ _id: userId }).select("role");
 
-      let Match = {};
-      if (UserRole.role !== "ADMIN") {
-        Match = { user_id: new ObjectId(userId) };
-      }
 
-      const userPayments = await PaymentModel.aggregate([
-        {
-          $match: Match
-        },
-        {
-          $sort: { createdAt: -1 }
-        },
-        {
-          $lookup: {
-            from: "products",
-            localField: "product_id",
-            foreignField: "_id",
-            as: "productDetails"
-          }
-        },
-        {
-          $unwind: "$productDetails"
-        },
-        {
-          $lookup: {
-            from: "users",
-            localField: "user_id",
-            foreignField: "_id",
-            as: "usersDetails"
-          }
-        },
-        {
-          $unwind: "$usersDetails"
-        },
-        {
-          $addFields: {
-            short_order_id: {
-              $cond: [
-                { $gt: [{ $strLenCP: "$order_id" }, 14] },
-                {
-                  $concat: [
-                    { $substr: ["$order_id", 0, 8] },
-                    "...",
-                    {
-                      $substr: [
-                        "$order_id",
-                        { $subtract: [{ $strLenCP: "$order_id" }, 6] },
-                        6
-                      ]
-                    }
-                  ]
-                },
-                "$order_id"
-              ]
-            }
-          }
-        },
-        {
-          $project: {
-            _id: 1,
-            order_id: 1,
-            short_order_id: 1,
-            amount: 1,
-            status: 1,
-            payment_method: 1,
-            createdAt: 1,
-            "productDetails.name": 1,
-            "usersDetails.FullName": 1
-          }
-        }
-      ]);
+  let Match = {};
+  if (UserRole.role === "ADMIN") {
 
-      res.status(200).json({
-        success: true,
-        message: `Payments for user ID: ${userId}`,
-        data: userPayments
-      });
-    } catch (error) {
-      console.error("Error fetching user payments:", error);
-      res.status(500).json({
-        success: false,
-        message: "Failed to fetch user payments",
-        error: error.message
-      });
-    }
+    Match = {};
+  }else{
+    Match = { user_id: new ObjectId(userId) };
   }
 
+
+  try {
+const userPayments = await PaymentModel.aggregate([
+  {
+    $match: Match
+  },
+  {
+    $sort: { createdAt: -1 }
+  },
+  {
+    $lookup: {
+      from: "products",             
+      localField: "product_id",   
+      foreignField: "_id",          
+      as: "productDetails"
+    }
+  },
+  {
+    $unwind: "$productDetails"      
+  },
+   {
+    $lookup: {
+      from: "users",             
+      localField: "user_id",   
+      foreignField: "_id",          
+      as: "usersDetails"
+    }
+  },
+  {
+    $unwind: "$usersDetails"      
+  },
+  {
+    $project: {
+      amount: 1,
+      status: 1,
+      payment_method: 1,
+      createdAt: 1,
+      "productDetails.name": 1  ,  
+      "usersDetails.FullName": 1     
+    }
+  }
+]);
+   
+
+    res.status(200).json({
+      success: true,
+      message: `Payments for user ID: ${userId}`,
+      data: userPayments,
+    });
+  } catch (error) {
+    console.error("Error fetching user payments:", error);
+    res.status(500).json({
+      success: false,
+      message: "Failed to fetch user payments",
+      error: error.message,
+    });
+  }
+}
 
 }
 
